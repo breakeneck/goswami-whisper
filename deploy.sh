e379ad65a80ed3725259b5b8deec7ae99398c463
Yuriy Datsenko <breakeneck@gmail.com>
2026-01-05 12:38:32 +0200

    Install required Python dependencies during deployment process

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 git fetch origin main
 git reset --hard origin/main
 
+./venv/bin/pip install -r requirements.txt
 docker compose up -d
 sleep 5  # Wait for MySQL to be ready
 ./venv/bin/flask db upgrade
